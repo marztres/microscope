567ae5e3a493abde4d6aeef5a553b97e5dfb2651
Marz Tres <marz13@outlook.com>
2015-06-20 17:57:46 -0500

    Conexión entre la colección Posts y la plantilla `postList`

--- client/templates/post/post_list.js
+++ client/templates/post/post_list.js
@@ -1,17 +1,3 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope by @cuantun',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor by @cuantun',
-    url: 'http://cuantun.meteor.com'
-  },
-  {
-    title: 'The Meteor Book @cuantun',
-    url: 'http://www.cuantun.com'
-  }
-];
 Template.postsList.helpers({
-  posts: postsData
+  posts: Posts.find()
 });
